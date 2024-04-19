a62b5c049a774fb29923e72d0583103602deefe5
murillotttorres@live.com <murillotttorres@live.com>
2024-04-18 21:23:02 -0300

    listagem de pets

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,30 +1,30 @@
-import  { createBrowserRouter } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { Pets } from "./pages/Pets"
-import { PetDetails } from "./pages/PetDetails"
+import { createBrowserRouter } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Pets } from './pages/Pets'
+import { PetDetails } from './pages/PetDetails'
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Home />
-    },
-    {
-        path: '/pets',
-        children: [
-            {
-                index: true,
-                element: <Pets />,
-            },
-            {
-                path: '/pets/:id',
-                element: <PetDetails />,
-            }
-        ]
-    },
-    {
-        path: '/admin',
-        element: <h1>Admin</h1>
-    }   
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/pets',
+    children: [
+      {
+        index: true,
+        element: <Pets />,
+      },
+      {
+        path: '/pets/:id',
+        element: <PetDetails />,
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    element: <h1>Admin</h1>,
+  },
 ])
 
 export default router
